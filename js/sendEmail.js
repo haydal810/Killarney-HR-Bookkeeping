8201f49dc2d3222ef22fd0a9fbf7e08c807a5079
David <haydal.tmps@gmail.com>
2019-12-12 19:50:18 +0000

    emailjs functionality added to contact form

---
+++ js/sendEmail.js
@@ -0,0 +1,21 @@
+function sendMail(contact) {
+    emailjs.send("gmail", "killarneyhr_website", {
+        "message": contact.message.value,
+        "from_name": contact.name.value,
+        "from_email": contact.emailaddress.value,
+        "from_phone_number": contact.phone.value,
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                setTimeout(function(){ alert("Message Sent"); }, 1000); // the user will know that their message was sent successfully and is given feedback.
+                document.getElementById("contactForm").reset() // the form is reset after submit button is hit
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page...
+}
+
+
